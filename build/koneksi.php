8a0e8012e25f97e6a20836abbc3eb0ff52c66ec5
Nenden Nuraeni <145337150+NendenNuraeni26@users.noreply.github.com>
2026-07-17 08:40:48 +0700

    Update koneksi.php

--- build/koneksi.php
+++ build/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("192.168.10.13", "teknis", "rsmtuban", "absen");
+$conn = new mysqli("192.168.x.x", "yawda", "yawda", "dbmu");
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
